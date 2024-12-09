8f9bb578f7758376ae7b1ac96e81bd702601c718
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2024-12-09 20:39:34 +0100

    use paths relative to repo root

--- bench.sh
+++ bench.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 # Expects the input for dayXY to be in ./build/inputXY
 day_command() {
-  printf "./build/d$1/d$1 b ./build/input$1"
+  printf "${SCRIPT_DIR}/build/d$1/d$1 b ${SCRIPT_DIR}/build/input$1"
 }
 
 declare -a bench_targets
 
 for i in $(seq 1 30); do
   printf -v d %02d $i
-  if ! [[ -f "./build/d$d/d$d" ]]; then
+  if ! [[ -f "${SCRIPT_DIR}/build/d$d/d$d" ]]; then
     continue
   fi
   bench_targets+=("$(day_command $d)")
@@ -18,6 +20,6 @@ done
 echo ${bench_targets[@]}
 
 hyperfine \
-  --export-markdown build/bench.md \
+  --export-markdown "${SCRIPT_DIR}/build/bench.md" \
   -N --prepare 'true' --warmup 1024 --min-runs 1024 \
   -- "${bench_targets[@]}"
