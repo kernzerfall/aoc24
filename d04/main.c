555f99687bc74419ae24a453e6fa651e47e3592a
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2024-12-09 17:09:25 +0100

    add d04p1

--- d04/main.c
+++ d04/main.c
@@ -1,3 +1,4 @@
+#define DEBUG
 #include "util/file.h"
 #include "util/log.h"
 #include "util/todo.h"
@@ -6,9 +7,119 @@
 #include <sys/mman.h>
 #include <sys/signal.h>
 
+static const char xmas[] = "XMAS";
+
+struct vec2 {
+	off_t x;
+	off_t y;
+};
+
+static const struct vec2 _DIR_E = { .x = 1, .y = 0 };
+static const struct vec2 _DIR_NE = { .x = 1, .y = 1 };
+static const struct vec2 _DIR_N = { .x = 0, .y = 1 };
+static const struct vec2 _DIR_NW = { .x = -1, .y = 1 };
+static const struct vec2 _DIR_W = { .x = -1, .y = 0 };
+static const struct vec2 _DIR_SW = { .x = -1, .y = -1 };
+static const struct vec2 _DIR_S = { .x = 0, .y = -1 };
+static const struct vec2 _DIR_SE = { .x = 1, .y = -1 };
+
+static const struct vec2 dirs[8] = { _DIR_E, _DIR_NE, _DIR_N, _DIR_NW,
+				     _DIR_W, _DIR_SW, _DIR_S, _DIR_SE };
+
+enum dir2 : u8 {
+	DIR_E = 0,
+	DIR_NE = 1,
+	DIR_N = 2,
+	DIR_NW = 3,
+	DIR_W = 4,
+	DIR_SW = 5,
+	DIR_S = 6,
+	DIR_SE = 7
+};
+
+struct ray {
+	struct vec2 pos;
+	struct vec2 dir;
+};
+
+void vec2_add(struct vec2 *res, const struct vec2 *v1, const struct vec2 *v2)
+{
+	res->x = v1->x + v2->x;
+	res->y = v1->y + v2->y;
+}
+
+int ray_advance(struct ray *ray, const struct vec2 *bounds)
+{
+	struct vec2 rpos;
+	vec2_add(&rpos, &ray->pos, &ray->dir);
+	if ((0 <= rpos.x) && (rpos.x < bounds->x) && (0 <= rpos.y) &&
+	    (rpos.y < bounds->y)) {
+		ray->pos = rpos;
+		return 0;
+	}
+	return -1;
+}
+
+size_t xy_to_offset(const struct vec2 *xy, const struct vec2 *bounds)
+{
+	return bounds->x * xy->y + xy->x;
+}
+
+void offset_to_xy(struct vec2 *res, size_t offset, const struct vec2 *bounds)
+{
+	res->x = offset % bounds->x;
+	res->y = offset / bounds->x;
+}
+
+bool xmas_ray(const char *__restrict__ buf, const struct vec2 *origin,
+	      const struct vec2 *bounds, enum dir2 dir)
+{
+	struct ray ray = { .pos = *origin, .dir = dirs[dir] };
+	for (size_t i = 0; i < 3; ++i) {
+		if (ray_advance(&ray, bounds) < 0 ||
+		    buf[xy_to_offset(&ray.pos, bounds)] != xmas[i + 1]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+size_t xmas_ray_scan(const char *__restrict__ buf, const struct vec2 *origin,
+		     const struct vec2 *bounds)
+{
+	size_t res = 0;
+	for (size_t i = 0; i < 8; ++i) {
+		res += xmas_ray(buf, origin, bounds, i);
+	}
+
+	return res;
+}
+
 void part1(char const *buf, size_t buf_size)
 {
-	TODO();
+	struct vec2 bounds = {
+		.x = line_length(buf, buf_size),
+		.y = count_lines(buf, buf_size),
+	};
+
+	pr_dbg_raw("bounds: %03zu - %03zu\n", bounds.x, bounds.y);
+
+	size_t res = 0;
+	for (size_t offset = 0; offset < buf_size; ++offset) {
+		if (buf[offset] != 'X') {
+			continue;
+		}
+
+		struct vec2 xy;
+		offset_to_xy(&xy, offset, &bounds);
+
+		pr_dbg_raw("found X at offset %03zu, xy = (%03zu, %03zu)\n",
+			   offset, xy.x, xy.y);
+
+		res += xmas_ray_scan(buf, &xy, &bounds);
+	}
+	pr_info("part1 result: %zu", res);
 }
 
 void part2(char const *buf, size_t buf_size)
