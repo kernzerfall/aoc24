a431ff944290ffeb63d30f98bb1434eea15562ce
kernzerfall <25184224+kernzerfall@users.noreply.github.com>
2024-12-09 23:08:46 +0100

    define sorting functions "with knowledge"

--- util/sort.c
+++ util/sort.c
@@ -23,3 +23,18 @@ void sort(void *arr, size_t arr_size, sort_cmp_f cmp, sort_xchg_f xchg)
 		}
 	}
 }
+
+void sort_knowl(void *knowl, void *arr, size_t arr_size, sort_cmp_knowl_f cmp,
+		sort_xchg_f xchg)
+{
+	bool changed = true;
+	while (changed) {
+		changed = false;
+		for (size_t i = 0; i < arr_size - 1; ++i) {
+			if (cmp(knowl, arr, i, i + 1) > 0) {
+				xchg(arr, i, i + 1);
+				changed = true;
+			}
+		}
+	}
+}
